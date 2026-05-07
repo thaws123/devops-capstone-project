96d705bf67d82af008272fddd3a20a4b0bcfad99
thaws123 <ty.haws@gmail.com>
2026-05-07 16:14:33 -0400

    implement list accounts endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -181,3 +181,11 @@ class TestAccountService(TestCase):
             content_type="application/json"
         )
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
